4ca99709322363f7b0a4949d31189248affc80fc
TrikayCoder <91646919+TrikayCoder@users.noreply.github.com>
2023-01-02 12:07:38 +0700

    add documents

--- src/main/java/TrikayDev/src/main/java/KayThread/KayThread.java
+++ src/main/java/TrikayDev/src/main/java/KayThread/KayThread.java
@@ -1,7 +1,12 @@
-package KayThread1;
+package KayThread;
 
 import java.util.concurrent.atomic.AtomicInteger;
-
+/**
+ * The KayThread class represents a thread that can be controlled by the user.
+ * It provides methods to start, stop, and finish the thread, as well as an
+ * interface to be implemented by the user to specify the code to be executed
+ * by the thread.
+ */
 public class KayThread {
     private static AtomicInteger nextId = new AtomicInteger();
     private int id;
@@ -13,20 +18,37 @@ public class KayThread {
 
     }
 
+    /**
+     * Finishes the thread. This method should be called when the thread has
+     * completed its execution.
+     */
     public void kayFinish() {
         kayThreadInterface.kayFinish(id);
     }
-
+    /**
+     * Stops the thread.
+     */
     public void kayStop() {
         isRunning = false;
     }
-
+    /**
+     * Stops the thread with the specified ID.
+     *
+     * @param id the ID of the thread to stop
+     */
     public void kayStop(int id) {
         if (this.id == id) {
             isRunning = false;
         }
     }
-
+    /**
+     * Starts the thread. The thread will execute the code specified by the
+     * KayThreadInterface provided as an argument.
+     *
+     * @param kayThreadInterface the interface containing the code to be executed
+     *                           by the thread
+     * @return the ID of the thread
+     */
     public int start(KayThreadInterface kayThreadInterface) {
         this.kayThreadInterface = kayThreadInterface;
         id = nextId.incrementAndGet();
--- src/main/java/TrikayDev/src/main/java/KayThread/KayThreadInterface.java
+++ src/main/java/TrikayDev/src/main/java/KayThread/KayThreadInterface.java
@@ -1,6 +1,20 @@
-package KayThread1;
-
+package KayThread;
+/**
+ * The KayThreadInterface interface should be implemented by any class that
+ * wants to create a KayThread. It contains two methods: kayRun() and
+ * kayFinish(). The kayRun() method specifies the code to be executed by the
+ * thread, while the kayFinish() method is called when the thread has finished
+ * executing.
+ */
 public interface KayThreadInterface {
+    /**
+     * Specifies the code to be executed by the thread.
+     */
     public void kayRun();
+    /**
+     * Called when the thread has finished executing.
+     *
+     * @param id the ID of the thread
+     */
     public void kayFinish(int id);
 }
